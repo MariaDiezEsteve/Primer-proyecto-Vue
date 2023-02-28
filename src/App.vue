0f2b2cfe9806a45448e58f6d646113c8510ef43b
MariaDiezEsteve <diezesteve@outlook.es>
2023-02-28 13:43:41 +0100

    primer contacto con Vue, están escrito los componentes y se llama a la App.vue al componente AboutUs

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Componentes del Grupo:</h1>
+  <AboutUs /> <!-- 3. Llamamos al componente aquí para que se muestre en el template HTML -->
+
+</template>
+
+<script>
+
+/* Para importar nuestro componente AboutUs */
+
+import AboutUs from './components/AboutUs' // 1. Importamos nuestro componente para poder llamarlo en el export y Template
+
+export default {
+components: {
+  AboutUs, // 2. Exportamos nuestro componente para poder utilizarlo en la parte de Template
+},
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/AboutUs.vue
@@ -0,0 +1,56 @@
+<template>
+    <h2>Sobre nosotros</h2>
+    <p> Estudiantes de {{ studies }}</p>
+    <p>Lugar de estudios: {{ city }}</p>
+    <p>{{ mensaje.title }} - {{ mensaje.text }}</p>
+
+    <div class="group">
+        <h5> {{ name[0] }} </h5>  <!-- 3. Recoger a la variable name para poder mostrarla  -->
+        <h5> {{ name[1] }} </h5>  <!-- 3. Recoger a la variable name para poder mostrarla  -->
+        <h5> {{ name[2] }} </h5>  <!-- 3. Recoger a la variable name para poder mostrarla  -->
+        <h5> {{ name[3] }} </h5>  <!-- 3. Recoger a la variable name para poder mostrarla  -->
+   </div>
+
+       <EachUs />
+  </template>
+  
+  <script>
+  import EachUs from './EachUs';
+  export default {
+    components: {
+      EachUs,
+    },
+
+    setup(){ // PARA INCLUIR EL COMPONENTE EN EL TEMPLATE 
+      const name = ["Juan", "María", "Gorka", "Gonzalo"]; // 1. Crear la variable name 
+      const studies = "Peñascal F5, Bootcamp FullStack Developer";
+      const city = "Bolueta"
+      const mensaje = {
+        title: "Empezamos en noviembre de 2022",
+        text: "Esperamos finalizar en julio de 2023",
+      }
+
+      return { //2. Devolver la variable name para poder llamarla en el template
+        name,
+        studies,
+        city,
+        mensaje,
+      }
+
+    }
+  
+  }
+  </script>
+  
+  <style>
+
+  .group{
+    display: flex;
+    justify-content: center;
+  }
+
+  h5{
+    padding:2rem;
+  }
+  
+  </style>
